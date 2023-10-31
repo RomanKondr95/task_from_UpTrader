d94e17fac4589c42afe0f14bedc81f788d532d27
RomanKondr95 <139111155+RomanKondr95@users.noreply.github.com>
2023-10-31 15:28:11 +0300

    исправил ошибку

--- myproject/menu/admin.py
+++ myproject/menu/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import MenuItem
 
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ['name', 'url', 'parent', 'order']
+    list_display = ['menu_name', 'url', 'parent', 'order']
     list_filter = ['parent']
 
 admin.site.register(MenuItem, MenuItemAdmin)
--- myproject/menu/models.py
+++ myproject/menu/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
+    # name = models.CharField(max_length=100)
+    menu_name = models.CharField(max_length=100,default='main_menu')
     url = models.CharField(max_length=100, blank=True, null=True)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
     order = models.PositiveIntegerField(default=0)
     menu_name = models.CharField(max_length=100,default='main_menu')
     def __str__(self):
-        return self.name
+        return self.menu_name
 
